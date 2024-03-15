04cb1351f6a4c9d5404f7adbc11d5c78a7211f08
zarifhaikalz <118897514+cebause01@users.noreply.github.com>
2024-03-15 14:07:53 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify
+from ultralytics import YOLO
+from ultralytics.models.yolo.detect.predict import DetectionPredictor
+import cv2
+
+
+app = Flask(__name__)
+
+model = YOLO("C:\\Users\\User\\Desktop\\APP\\Basic-1st.pt")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def start_detection():
+    try:
+        model.predict(source="0", show=True, conf=0.6)
+        return render_template('completed.html')
+    except Exception as e:
+        return render_template('error.html', error_message=str(e))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
